555c505e3adc15483077577c90fdad7c5d741733
Emeka Edwin Asoluka <asolukaedwin@gmail.com>
2024-11-09 18:52:37 -0700

    updated implementation

--- Insertion_Sort/InsertionSort.java
+++ Insertion_Sort/InsertionSort.java
@@ -1,30 +1,20 @@
 public class InsertionSort{
   public static void main(String args[]){
     int[] list = {4,25,3,5,2,1,6};
-
-        int currentValue;
-        int n = list.length;
-        int insertIndex;
-        for(int i=0; i<n-1; i++){
-            for(int num: list){
-                System.out.print(num + " ");
-            }
-            System.out.println();
-          currentValue = list[i+1];
-          for( int j=i; j>=0; j--){
-            if(list[j]>currentValue){
-              list[j+1] = list[j];
-              if(j==0)
-                list[j] = currentValue;
-            }
-            else{
-              list[j+1] = currentValue;
-              break;
-            }
-          }
+        
+    int currSortedIndex;
+    int currentValue;
+    for(int sortedEnd=0; sortedEnd<list.length-1; sortedEnd++){
+        currentValue = list[sortedEnd+1];
+        currSortedIndex = sortedEnd;
+        while(currSortedIndex>=0 && list[currSortedIndex]>currentValue){
+            list[currSortedIndex+1] = list[currSortedIndex];
+            currSortedIndex--;
         }
-        for(int num: list){
-            System.out.print(num + " ");
-        }
+        list[currSortedIndex+1] = currentValue;
+    }
+    for(int num: list){
+        System.out.print(num+" ");
+    }
   }
 }
